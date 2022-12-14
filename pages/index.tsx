e4b80f2959a6d8876e8b095cca3293b8f82fd655
michaeldamage <michaeldamagesteam@gmail.com>
2022-12-14 15:19:43 +0100

    runfix: remove not defined layout component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,4 @@
 import Head from "next/head";
-import { Layout } from "../components/Layout/Layout";
-import { LoginForm } from "../components/LoginForm/LoginForm";
 import styles from "../styles/Home.module.scss";
 
 export default function Home() {
@@ -11,11 +9,9 @@ export default function Home() {
         <meta name="description" content="Managing palisades the good way." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Layout>
-        <div className={styles.container}>
-          <h1 className={styles.title}>FenceLane</h1>
-        </div>
-      </Layout>
+      <div className={styles.container}>
+        <h1 className={styles.title}>FenceLane</h1>
+      </div>
     </>
   );
 }
