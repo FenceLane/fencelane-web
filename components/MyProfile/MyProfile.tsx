de30278a8e3e3ac6f650377c3ae822b56fccc2da
michaeldamage <michaeldamagesteam@gmail.com>
2023-01-03 12:59:18 +0100

    feat: created MyProfile component

---
+++ components/MyProfile/MyProfile.tsx
@@ -0,0 +1,87 @@
+import { Button } from "@chakra-ui/react";
+import Head from "next/head";
+import { useRouter } from "next/router";
+import React from "react";
+import { apiClient } from "../../lib/api/apiClient";
+import { useContent } from "../../lib/hooks/useContent";
+import { mapAxiosErrorToLabel } from "../../lib/server/BackendError/BackendError";
+import { timeToDate } from "../../lib/util/dates";
+import { toastError, toastInfo } from "../../lib/util/toasts";
+import { Layout } from "../Layout/Layout";
+import { InferGetServerSidePropsType } from "next";
+
+export const MyProfile = ({
+  user,
+  session,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  const { t } = useContent();
+  const router = useRouter();
+
+  const handleLogout = () => {
+    apiClient.auth
+      .deleteLogout()
+      .then(() => toastInfo(t("success.logout")))
+      .catch((error) => {
+        toastError(
+          t(`errors.backendErrorLabel.${mapAxiosErrorToLabel(error)}`)
+        );
+      })
+      .finally(() => {
+        router.push("/login");
+      });
+  };
+
+  const handleDeleteAccount = () => {
+    apiClient.auth
+      .deleteSelfUser()
+      .then(() => toastInfo(t("success.delete")))
+      .catch((error) => {
+        toastError(
+          t(`errors.backendErrorLabel.${mapAxiosErrorToLabel(error)}`)
+        );
+      })
+      .finally(() => {
+        router.push("/login");
+      });
+  };
+
+  return (
+    <>
+      <Head>
+        <title>FenceLane</title>
+        <meta name="description" content="Managing palisades the good way." />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Layout user={user}>
+        <main>
+          <h2>Mój profil</h2>
+          <h3>Dane użytkownika:</h3>
+          <pre>
+            <p>id: {user.id}</p>
+            <p>nazwa: {user.name}</p>
+            <p>email: {user.email}</p>
+            <p>telefon: {user.phone}</p>
+            <p>rola: {user.role}</p>
+          </pre>
+          <h3>Informacje o bieżącej sessji:</h3>
+          <pre>
+            <p>id: {session.id}</p>
+            <p>Data utworzenia: {timeToDate(session.createdAt)}</p>
+            <p>Data odświeżenia: {timeToDate(session.updatedAt)}</p>
+            <p>Data wygaśnięcia: {timeToDate(session.expiresAt)}</p>
+          </pre>
+          <Button onClick={handleLogout} colorScheme="teal" variant="outline">
+            wyloguj się
+          </Button>
+          <Button
+            onClick={handleDeleteAccount}
+            colorScheme="red"
+            variant="outline"
+          >
+            usuń konto
+          </Button>
+        </main>
+      </Layout>
+    </>
+  );
+};
--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -1,5 +1,4 @@
 import { Button } from "@chakra-ui/react";
-import { InferGetServerSidePropsType } from "next";
 import Head from "next/head";
 import { useRouter } from "next/router";
 import { Layout } from "../../components/Layout/Layout";
@@ -10,88 +9,23 @@ import { withTranslationProps } from "../../lib/server/middlewares/withTranslati
 import { timeToDate } from "../../lib/util/dates";
 import { useContent } from "../../lib/hooks/useContent";
 import { toastError, toastInfo } from "../../lib/util/toasts";
+import { MyProfile } from "../../components/MyProfile/MyProfile";
 
-export default function Home({
-  user,
-  session,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
-  const { t } = useContent();
-  const router = useRouter();
-
-  const handleLogout = () => {
-    apiClient.auth
-      .deleteLogout()
-      .then(() => toastInfo(t("success.logout")))
-      .catch((error) => {
-        toastError(
-          t(`errors.backendErrorLabel.${mapAxiosErrorToLabel(error)}`)
-        );
-      })
-      .finally(() => {
-        router.push("/login");
-      });
-  };
-
-  const handleDeleteAccount = () => {
-    apiClient.auth
-      .deleteSelfUser()
-      .then(() => toastInfo(t("success.delete")))
-      .catch((error) => {
-        toastError(
-          t(`errors.backendErrorLabel.${mapAxiosErrorToLabel(error)}`)
-        );
-      })
-      .finally(() => {
-        router.push("/login");
-      });
-  };
-
+const MyProfilePage = () => {
+  const { t } = useContent("pages.profile");
   return (
-    <>
-      <Head>
-        <title>FenceLane</title>
-        <meta name="description" content="Managing palisades the good way." />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Layout user={user}>
-        <main>
-          <h2>Mój profil</h2>
-          <h3>Dane użytkownika:</h3>
-          <pre>
-            <p>id: {user.id}</p>
-            <p>nazwa: {user.name}</p>
-            <p>email: {user.email}</p>
-            <p>telefon: {user.phone}</p>
-            <p>rola: {user.role}</p>
-          </pre>
-          <h3>Informacje o bieżącej sessji:</h3>
-          <pre>
-            <p>id: {session.id}</p>
-            <p>Data utworzenia: {timeToDate(session.createdAt)}</p>
-            <p>Data odświeżenia: {timeToDate(session.updatedAt)}</p>
-            <p>Data wygaśnięcia: {timeToDate(session.expiresAt)}</p>
-          </pre>
-          <Button onClick={handleLogout} colorScheme="teal" variant="outline">
-            wyloguj się
-          </Button>
-          <Button
-            onClick={handleDeleteAccount}
-            colorScheme="red"
-            variant="outline"
-          >
-            usuń konto
-          </Button>
-        </main>
-      </Layout>
-    </>
+    <Layout>
+      <MyProfile user={props.user} session={props.session} />
+    </Layout>
   );
-}
+};
+
+export default MyProfilePage;
 
 export const getServerSideProps = withTranslationProps(
   withServerSideAuth(async (ctx) => {
     const { user, ...session } = ctx.session;
     const { id, createdAt, updatedAt, expiresAt } = session;
-
     return {
       props: {
         user,
