cd23faff0e71d190f15df089e712f6760c20c23a
michaeldamage <michaeldamagesteam@gmail.com>
2023-03-07 09:14:46 +0100

    feat: add OrderAddModal

---
+++ components/Orders/OrderAddModal/OrderAddModal.tsx
@@ -0,0 +1,80 @@
+import {
+  Modal,
+  ModalContent,
+  ModalOverlay,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  Button,
+  ModalFooter,
+} from "@chakra-ui/react";
+import React, { useState } from "react";
+import { useContent } from "../../../lib/hooks/useContent";
+import styles from "./OrderAddModal.module.scss";
+import { usePostOrder } from "../../../lib/api/hooks/orders";
+
+const initialOrderState = {};
+
+interface OrderAddModalProps {
+  onAddClose: Function;
+  isAddOpen: boolean;
+}
+
+export const OrderAddModal = ({
+  onAddClose,
+  isAddOpen,
+}: OrderAddModalProps) => {
+  const { t } = useContent();
+
+  const [orderData, setOrderData] = useState(initialOrderState);
+
+  const handleModalClose = () => {
+    onAddClose();
+    setOrderData(initialOrderState);
+  };
+
+  const {
+    mutate: postOrder,
+    error,
+    isSuccess,
+    isLoading,
+  } = usePostOrder(handleModalClose);
+
+  const handlePostOrder = () => {
+    const {} = orderData;
+
+    postOrder({});
+  };
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setOrderData((orderData) => ({
+      ...orderData,
+      [name]: value,
+    }));
+  };
+
+  return (
+    <Modal isOpen={isAddOpen} onClose={handleModalClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>{t("pages.storage.modals.commodity_adding")}</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody className={styles["modal-inputs"]}></ModalBody>
+        <ModalFooter alignItems="flex-end">
+          <Button
+            colorScheme="green"
+            isLoading={isLoading}
+            onClick={handlePostOrder}
+            mr={3}
+          >
+            {t("pages.storage.buttons.add")}
+          </Button>
+          <Button colorScheme="red" onClick={handleModalClose}>
+            {t("pages.storage.buttons.cancel")}
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+};
--- components/Orders/Orders.tsx
+++ components/Orders/Orders.tsx
@@ -1,9 +1,10 @@
 import React from "react";
-import { Button, Flex, Text } from "@chakra-ui/react";
+import { Button, Flex, Text, useDisclosure } from "@chakra-ui/react";
 import { OrderInfo } from "../../lib/types";
 import { OrdersRow } from "./OrdersRow/OrdersRow";
 import { AddIcon } from "@chakra-ui/icons";
 import { useContent } from "../../lib/hooks/useContent";
+import { OrderAddModal } from "./OrderAddModal/OrderAddModal";
 // import styles from "./Orders.module.scss";
 
 interface OrderProps {
@@ -12,6 +13,13 @@ interface OrderProps {
 
 export const Orders = ({ orders }: OrderProps) => {
   const { t } = useContent();
+
+  const {
+    isOpen: isAddOpen,
+    onOpen: onAddOpen,
+    onClose: onAddClose,
+  } = useDisclosure();
+
   return (
     <>
       <Text color="var(--dark)" fontSize="20px" fontWeight="500" m="10px">
@@ -24,6 +32,7 @@ export const Orders = ({ orders }: OrderProps) => {
           fontWeight="400"
           h="32px"
           m="0 10px 10px 0"
+          onClick={onAddOpen}
         >
           {t("pages.orders.buttons.new")}
           <AddIcon ml="10px" />
@@ -32,6 +41,7 @@ export const Orders = ({ orders }: OrderProps) => {
       {Object.values(orders)[0].map((order: any) => (
         <OrdersRow key={order.products[0].orderId} orderData={order} />
       ))}
+      <OrderAddModal onAddClose={onAddClose} isAddOpen={isAddOpen} />
     </>
   );
 };
