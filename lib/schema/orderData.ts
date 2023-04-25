607fb914f087b8c5f52f43bc984b724dad1eccdd
PatrykBuniX <patrykbunix@gmail.com>
2023-04-25 18:56:51 +0200

    feat: add optional profit field to order

--- lib/schema/orderData.ts
+++ lib/schema/orderData.ts
@@ -6,6 +6,7 @@ export const OrderDataSchema = z.object({
   clientId: z.string().min(1),
   destinationId: z.string().min(1),
   date: z.date().optional(),
+  profit: z.number().optional(),
   status: z.nativeEnum(ORDER_STATUS).optional(),
   files: z.array(z.string()).optional(),
   products: z.array(ProductOrderDataSchema.omit({ orderId: true })),
