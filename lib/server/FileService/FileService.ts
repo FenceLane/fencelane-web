373218c5d4f6bbf66f191defe3aa1e90ee4c0081
PatrykBuniX <patrykbunix@gmail.com>
2023-04-29 22:13:27 +0200

    feat: save entry in db after uploading a file to s3

--- lib/server/FileService/FileService.ts
+++ lib/server/FileService/FileService.ts
@@ -2,20 +2,26 @@ import AWS from "aws-sdk";
 import { File } from "formidable";
 import fs from "fs";
 
-const s3 = new AWS.S3({
-  accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY,
-});
-
-export const uploadFile = async (file: File) => {
-  const filePath = file.filepath;
-  const fileBlob = fs.readFileSync(filePath);
+const initAWSBucket = () => {
+  const client = new AWS.S3({
+    accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY,
+  });
 
   const bucketName = process.env.AWS_S3_BUCKET_NAME;
 
   if (!bucketName) {
     throw Error("AWS_S3_BUCKET_NAME env variable was not provided.");
   }
+
+  return { client, bucketName };
+};
+
+const { client: s3, bucketName } = initAWSBucket();
+
+export const uploadFile = async (file: File) => {
+  const filePath = file.filepath;
+  const fileBlob = fs.readFileSync(filePath);
 
   const uploadedImage = await s3
     .upload({
--- pages/api/orders/[orderId]/files/index.ts
+++ pages/api/orders/[orderId]/files/index.ts
@@ -2,7 +2,14 @@ import { withApiAuth } from "../../../../../lib/server/middlewares/withApiAuth";
 import { withApiMethods } from "../../../../../lib/server/middlewares/withApiMethods";
 import { uploadFile } from "../../../../../lib/server/FileService/FileService";
 import { withValidatedFormDataBody } from "../../../../../lib/server/middlewares/withValidatedFormDataBody";
-import { BackendResponseStatusCode } from "../../../../../lib/server/BackendError/BackendError";
+import {
+  BackendErrorLabel,
+  BackendResponseStatusCode,
+  sendBackendError,
+} from "../../../../../lib/server/BackendError/BackendError";
+import { prismaClient } from "../../../../../lib/prisma/prismaClient";
+import { PrismaErrorCode } from "../../../../../lib/server/BackendError/BackendError";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
 
 // we want to parse the form data body ourselves (withValidatedFormDataBody)
 export const config = {
@@ -19,34 +26,57 @@ export default withApiMethods({
         throw Error('"orderId" was not passed in dynamic api path.');
       }
 
-      const uploadedFiles = await Promise.all(req.files.map(uploadFile));
+      try {
+        const uploadedFiles = await Promise.all(req.files.map(uploadFile));
 
-      //@TODO: save uploadedFiles reference to database
-      return res
-        .status(BackendResponseStatusCode.SUCCESS)
-        .send({ data: uploadedFiles });
+        await prismaClient.orderFile.createMany({
+          data: uploadedFiles.map((file) => ({
+            orderId: Number(orderId),
+            key: file.Key,
+            url: file.Location,
+          })),
+          skipDuplicates: true, //if file already exists (based on key field), skip it
+        });
+
+        const responseData = uploadedFiles.map((file) => ({
+          key: file.Key,
+          url: file.Location,
+        }));
+
+        return res
+          .status(BackendResponseStatusCode.SUCCESS)
+          .send({ data: responseData });
+      } catch (error) {
+        if (error instanceof PrismaClientKnownRequestError) {
+          if (error.code === PrismaErrorCode.FOREIGN_KEY_NOT_FOUND) {
+            return sendBackendError(res, {
+              code: BackendResponseStatusCode.NOT_FOUND,
+              label: BackendErrorLabel.ORDER_DOES_NOT_EXIST,
+              message: error.message,
+            });
+          }
+        }
+
+        return sendBackendError(res, {
+          code: BackendResponseStatusCode.BAD_REQUEST,
+          label: BackendErrorLabel.FILE_UPLOAD_ERROR,
+        });
+      }
     })
   ),
 
-  //   GET: withApiAuth(async (req, res) => {
-  //     const { orderId } = req.query;
-  //     if (typeof orderId !== "string") {
-  //       throw Error('"orderId" was not passed in dynamic api path.');
-  //     }
+  GET: withApiAuth(async (req, res) => {
+    const { orderId } = req.query;
+    if (typeof orderId !== "string") {
+      throw Error('"orderId" was not passed in dynamic api path.');
+    }
 
-  //     const orderExpanses = await prismaClient.productExpanse.findMany({
-  //       where: { productOrder: { orderId: Number(orderId) } },
-  //       select: {
-  //         id: true,
-  //         price: true,
-  //         currency: true,
-  //         type: true,
-  //         productOrderId: true,
-  //       },
-  //     });
+    const orderFiles = await prismaClient.orderFile.findMany({
+      where: { orderId: Number(orderId) },
+    });
 
-  //     return res
-  //       .status(BackendResponseStatusCode.SUCCESS)
-  //       .send({ data: orderExpanses });
-  //   }),
+    return res
+      .status(BackendResponseStatusCode.SUCCESS)
+      .send({ data: orderFiles });
+  }),
 });
