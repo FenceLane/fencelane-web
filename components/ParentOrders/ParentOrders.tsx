8e10e16ff5bcec3e8ab6347e4c21e1c83ec26950
michaeldamage <michaeldamagesteam@gmail.com>
2023-07-03 13:40:58 +0200

    moved: content to common

--- components/ParentOrders/ParentOrders.tsx
+++ components/ParentOrders/ParentOrders.tsx
@@ -4,6 +4,7 @@ import { ParentOrdersRow } from "./ParentOrdersRow/ParentOrdersRow";
 import { OrderInfo } from "../../lib/types";
 import styles from "./ParentOrders.module.scss";
 import { useIsMobile } from "../../lib/hooks/useIsMobile";
+import { useContent } from "../../lib/hooks/useContent";
 
 interface ParentOrdersProps {
   ordersData: { [ParentOrderId: string]: OrderInfo[] };
@@ -11,16 +12,25 @@ interface ParentOrdersProps {
 
 export const ParentOrders = ({ ordersData }: ParentOrdersProps) => {
   const isMobile = useIsMobile();
+  const { t } = useContent();
 
   return (
     <Flex bg="white" className={styles["wrapper"]}>
       <Table className={styles["orders-table"]}>
         <Thead borderBottom="3px solid var(--light-content)">
           <Tr>
-            <Th>{isMobile ? "Nr zamówienia".slice(0, -5) : "Nr zamówienia"}</Th>
-            <Th>{isMobile ? "Nr załadunku".slice(0, -5) : "Nr załadunku"}</Th>
-            <Th>Produkty</Th>
-            <Th>Ilość</Th>
+            <Th>
+              {isMobile
+                ? t("pages.orders.order.parent_order_id").slice(0, -5)
+                : t("pages.orders.order.parent_order_id")}
+            </Th>
+            <Th>
+              {isMobile
+                ? t("pages.orders.order.order_id").slice(0, -4)
+                : t("pages.orders.order.order_id")}
+            </Th>
+            <Th>{t("pages.orders.order.products")}</Th>
+            <Th>{t("main.quantity")}</Th>
           </Tr>
         </Thead>
         {Object.entries(ordersData).map((value) => (
