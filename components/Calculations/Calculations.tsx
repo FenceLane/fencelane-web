97d4cf93e6b27a26962cbce880c760e382215b46
michaeldamage <michaeldamagesteam@gmail.com>
2023-07-13 18:20:43 +0200

    chore: standarized border radiuses and boxshadows

--- components/Calculations/Calculations.tsx
+++ components/Calculations/Calculations.tsx
@@ -1,6 +1,16 @@
 import React from "react";
 import { OrderInfo } from "../../lib/types";
-import { Box, Text, Table, Td, Th, Tr, Button } from "@chakra-ui/react";
+import {
+  Box,
+  Text,
+  Table,
+  Td,
+  Th,
+  Tr,
+  Button,
+  Thead,
+  Tbody,
+} from "@chakra-ui/react";
 import styles from "./Calculations.module.scss";
 import { useContent } from "../../lib/hooks/useContent";
 import Link from "next/link";
@@ -17,39 +27,47 @@ export const Calculations = ({ orderData }: CalculationsProps) => {
       <Text color="var(--dark)" fontSize="20px" fontWeight="500" m="10px">
         {t("pages.orders.calculations_history")}
       </Text>
-      <Box className={styles.container}>
-        <Table className={styles["calc-table"]}>
-          <Tr>
-            <Th textAlign="center">{t("pages.orders.order.order_id")}</Th>
-            <Th textAlign="center">{t("pages.orders.order.profit")} [€]</Th>
-            <Th textAlign="center"></Th>
-          </Tr>
+      <Box
+        className={styles.container}
+        boxShadow="0px 4px 4px rgba(0, 0, 0, 0.15)"
+        borderRadius="5px"
+      >
+        <Table className={styles["calc-table"]} borderRadius="5px">
+          <Thead>
+            <Tr borderRadius="5px">
+              <Th textAlign="center">{t("pages.orders.order.order_id")}</Th>
+              <Th textAlign="center">{t("pages.orders.order.profit")} [€]</Th>
+              <Th textAlign="center"></Th>
+            </Tr>
+          </Thead>
           {orderData
             .filter((order) => order.profit !== null)
             .map((order) => (
-              <Tr key={order.id} textAlign="center">
-                <Td textAlign="center">
-                  {order.id.toString().padStart(4, "0")}
-                </Td>
-                <Td textAlign="center">
-                  {Number(order.profit).toFixed(2).replace(/\.00$/, "")} €
-                </Td>
-                <Td>
-                  <Link
-                    className={styles["details-link"]}
-                    href={`/calculations/${order.id}`}
-                  >
-                    <Button
-                      color="white"
-                      bg=""
-                      className={styles["calc-button"]}
+              <Tbody key={order.id}>
+                <Tr textAlign="center">
+                  <Td textAlign="center">
+                    {order.id.toString().padStart(4, "0")}
+                  </Td>
+                  <Td textAlign="center">
+                    {Number(order.profit).toFixed(2).replace(/\.00$/, "")} €
+                  </Td>
+                  <Td>
+                    <Link
+                      className={styles["details-link"]}
+                      href={`/calculations/${order.id}`}
                     >
-                      {t("pages.orders.order.calculation")}
-                      <ArrowForwardIcon />
-                    </Button>
-                  </Link>
-                </Td>
-              </Tr>
+                      <Button
+                        color="white"
+                        bg=""
+                        className={styles["calc-button"]}
+                      >
+                        {t("pages.orders.order.calculation")}
+                        <ArrowForwardIcon />
+                      </Button>
+                    </Link>
+                  </Td>
+                </Tr>
+              </Tbody>
             ))}
         </Table>
       </Box>
