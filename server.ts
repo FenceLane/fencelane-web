cff2456f5de65bc39322464fef99a360c460d7be
PatrykBuniX <patrykbunix@gmail.com>
2023-07-14 20:42:42 +0200

    feat: add custom server logs

--- server.ts
+++ server.ts
@@ -24,6 +24,9 @@ const httpsOptions = {
 };
 
 app.prepare().then(() => {
+  setInterval(() => {
+    console.log("patryk custom server log interval");
+  }, 5000);
   const server = createServer(httpsOptions, async (req, res) => {
     try {
       const { url } = req;
